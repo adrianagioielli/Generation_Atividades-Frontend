1f59dc4cf8e1494355145d64e8056cd87147aad6
Adriana Gioielli <adrianalves.cp@gmail.com>
2023-10-16 08:07:15 -0300

    task2
    uso de props

---
+++ tasks_semana11_e_12_202310/task02/src/App.tsx
@@ -0,0 +1,12 @@
+import Home from './pages/home/Home';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+   <Home  title="Componente Home"
+          description="Este é um componente Home que recebe props."/>
+  );
+}
+
+export default App;
