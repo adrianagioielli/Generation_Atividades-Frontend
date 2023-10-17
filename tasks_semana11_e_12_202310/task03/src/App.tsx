c151dcb71c88fd59ccdeb7703734cd9cd90bf81a
Adriana Gioielli <adrianalves.cp@gmail.com>
2023-10-17 01:06:25 -0300

    task 03
    componentes menu e rodapé

--- tasks_semana11_e_12_202310/task03/src/App.tsx
+++ tasks_semana11_e_12_202310/task03/src/App.tsx
@@ -2,29 +2,16 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
-import Login from './paginas/login/Login';
-import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-  
+    <Navbar/>
+    <Home/>
+    <Footer/>
     </>
-  );
+);
 }
 export default App;
